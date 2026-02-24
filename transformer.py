156e905ce42ac2526eacb9222c01ad4181ff339e
Hemanthsp999 <hemanth.sp03@gmail.com>
2026-02-24 23:11:07 +0530

    -added positional encoding.

--- transformer.py
+++ transformer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import tiktoken
+import math
 
 encoder = tiktoken.get_encoding("gpt2")
 
@@ -22,18 +23,35 @@ class InputTokens:
 class InputEmbeddings(nn.Module):
     def __init__(self, vocab_size, d_size):
         super().__init__()
-        self.embeddings = nn.Embedding(vocab_size, d_size)
+        self.d_model = d_size
+        self.embeddings = nn.Embedding(vocab_size, d_size) # original papper shows dimension of vocab_size * 512 
 
     def forward(self, indices):
-        return self.embeddings(indices)
+        return self.embeddings(indices) * math.sqrt(self.d_model) # matrix * d_model
 
 
-query = "what is your name"
+class PositionalEncoding(nn.Module):
 
-query_encoder = InputTokens(query)
-tokens = query_encoder.encode()  # Returns a list of ints
+    def __init__(self, d_model: int, dropout: float, seq_length: int):
+        super().__init__()
 
-token_tensor = torch.tensor(tokens, dtype=torch.long)
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+        self.seq_length = seq_length
 
-embeddings = InputEmbeddings(50257, 412)
-print(embeddings.forward(token_tensor))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # 10000 ^ (2 * i)/d_model
+
+        self.pe = torch.zeros(seq_length, d_model) # create tensor of size seq_lenth * d_model
+        self.pos = torch.arange(0, seq_length, dtype=torch.float).unsqueeze(1) # create a tensor of 0 to seqlength of data filled 1
+
+        self.pe[::2] = torch.sin(pos * div_term) # for even pos
+        self.pe[1::2] = torch.cos(pos * div_term) # for odd pos
+
+
+        self.pe = self.pe.unsqueeze(0) # (1, seq_length, d_model)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[: ,:x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
