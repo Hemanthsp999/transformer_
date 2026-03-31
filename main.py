5954532612c848c576973a4262524caa361c6777
Hemanthsp999 <hemanth.sp03@gmail.com>
2026-03-31 23:30:13 +0530

    -working on integrating tokenzier

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import torch
 import torch.nn as nn
-from datasets import load_dataset
-from tokenizer import Tokenizer
-from tokenizer.models import WordLevel
-from tokenizer.trainers import WordLevelTrainer
-from tokenizer.pre_tokenizers import Whitespace
-from pathlib import Path
 
-def get_or_build_tokenizer(config, ds, lang):
 
-    tokenizer_path = Path(config['tokenizer_file'].format(lang))
-
-    if not Path.exists(tokenizer_path):
-        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
-        tokenizer_pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens=["UNK", "PAD", "SOS", "EOS"], min_frequency=2)
-        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
-        tokenizer.save(str(tokenizer_path))
-        
+def build_tokenizer():
+    print("building tokenizer")
